b0e8047aebfdbfd67494f3f6b2740dd34cdba724
JohannaGonzalez <jg140301@gmail.com>
2025-07-09 10:44:18 -0400

    'Aprendiendo dart'

--- bin/dart_basics/bin/dart_basics.dart
+++ bin/dart_basics/bin/dart_basics.dart
@@ -1,13 +1,14 @@
 import 'dart:io';
+// ignore: unused_import
 import 'package:dart_basics/dart_basics.dart' as dart_basics;
 
 void main(List<String> arguments) {
-  // print('Introduce tu año de nacimiento!');
-  // String date = stdin.readLineSync()!;//este codigo (readLineSyn) permmite meter datos desde afuera, como un input.
-  // const int currentYear = 2025;
-  // int formatDate = int.parse(date);
-  // int result = currentYear - formatDate;
-  // print("Tienes $result años");
+  print('Introduce tu año de nacimiento!');
+  String date = stdin.readLineSync()!;//este codigo (readLineSyn) permmite meter datos desde afuera, como un input.
+  const int currentYear = 2025;
+  int formatDate = int.parse(date);
+  int result = currentYear - formatDate;
+  print("Tienes $result años");
 
  int userAge = 18;
 
