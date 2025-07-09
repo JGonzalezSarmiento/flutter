635462c6aa499adefeb9872dfeb52d54b8d83841
JohannaGonzalez <jg140301@gmail.com>
2025-07-09 10:33:45 -0400

    'Aprendiendo Dart'

---
+++ bin/dart_basics/bin/dart_basics.dart
@@ -0,0 +1,25 @@
+import 'dart:io';
+import 'package:dart_basics/dart_basics.dart' as dart_basics;
+
+void main(List<String> arguments) {
+  // print('Introduce tu año de nacimiento!');
+  // String date = stdin.readLineSync()!;//este codigo (readLineSyn) permmite meter datos desde afuera, como un input.
+  // const int currentYear = 2025;
+  // int formatDate = int.parse(date);
+  // int result = currentYear - formatDate;
+  // print("Tienes $result años");
+
+ int userAge = 18;
+
+ if (userAge >= 18){
+    print("Eres mayor de edad");  
+ } else {
+    print("Eres menor de edad");
+ }
+
+
+
+
+
+
+}
